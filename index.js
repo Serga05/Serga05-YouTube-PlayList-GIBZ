a731b2334d912d02195102d6810616f6125dd2a5
Serga05 <88805832+Serga05@users.noreply.github.com>
2021-12-09 18:16:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+       function COINSdrop(){
+		document.write('<a href="http://Serga05.GitHub.io/hah"><button id="ButtonBack"><-- Назад</button></a><P>Если у видео нет монетизации то реклама не будет мешать просмотру видео!<body class="body-color-black"></body><link href="index.css" rel="stylesheet"></link>');
+	   }
+       function PUBLISHvideo(){
+		   document.write('<link href="index.css" rel="stylesheet"></link><body class="body-color-black"></body>Вы не можете опублековать видео тут это может делать только администратор но если вы сможете связатся со мною то возможно мы сможем договориться о опублекование тут!<a href="http://t.me/Smailiki0281"><button id="button-MYtelegram">Мой телеграмм - Serga05</button></a>')
+	   }
