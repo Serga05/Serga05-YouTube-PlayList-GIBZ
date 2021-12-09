e90cff9a90d035264b957b9cd9b9bccf527952af
Serga05 <88805832+Serga05@users.noreply.github.com>
2021-12-09 17:01:48 +0300

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-    function What(){
-		document.write('<A HREF="Serga05.github.io/hah"><button id="ButtonBack"><-- Назад</button></a><p><link rel="stylesheet" href="index.css" type="text/css"></link>Если видео без монитизации значит реклама не будет мешать вашему просмотру!</p><p>Советую смотреть это видео именно здесь ведь в YouTube будет мешать реклама в начале!</p>')
-	}
