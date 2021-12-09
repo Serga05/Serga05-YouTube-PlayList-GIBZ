465196653336ef5cff4e42eda74978a7b17626f2
Serga05 <88805832+Serga05@users.noreply.github.com>
2021-12-09 17:03:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+       function COINSdrop(){
+		document.write('<a href="http://Serga05.GitHub.io/hah"><button id="ButtonBack"><-- Назад</button></a><P>Если у видео нет монетизации то реклама не будет мешать просмотру видео!<body class="body-color-black"></body><link href="index.css" rel="stylesheet"></link>');
+	   }
