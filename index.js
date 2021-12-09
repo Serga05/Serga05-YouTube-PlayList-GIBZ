11e10744a4214fa03e97ff6c0a1b0bc5df79a044
Serga05 <88805832+Serga05@users.noreply.github.com>
2021-12-09 18:14:42 +0300

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-       function COINSdrop(){
-		document.write('<a href="http://Serga05.GitHub.io/hah"><button id="ButtonBack"><-- Назад</button></a><P>Если у видео нет монетизации то реклама не будет мешать просмотру видео!<body class="body-color-black"></body><link href="index.css" rel="stylesheet"></link>');
-	   }
